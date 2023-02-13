8bf093df1b9fe7582b2e83e83c6a684fa04c6109
Andrey <pinchuk.andryusha310212@gmail.com>
2023-02-13 14:39:40 +0200

    feat: add repositories

---
+++ src/main/java/ua/kishkastrybaie/repository/VariationRepository.java
@@ -0,0 +1,9 @@
+package ua.kishkastrybaie.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ua.kishkastrybaie.repository.entity.Variation;
+
+@Repository
+public interface VariationRepository extends JpaRepository<Variation, Long> {
+}
