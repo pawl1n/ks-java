46bbd2c39823a3d387d67df34212c568e1ba99fc
Andrey <pinchuk.andryusha310212@gmail.com>
2023-02-22 19:57:49 +0200

    feat: add repositories

---
+++ src/main/java/ua/kishkastrybaie/repository/CartItemRepository.java
@@ -0,0 +1,9 @@
+package ua.kishkastrybaie.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ua.kishkastrybaie.repository.entity.CartItem;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+}
---
+++ src/main/java/ua/kishkastrybaie/repository/CartRepository.java
@@ -0,0 +1,9 @@
+package ua.kishkastrybaie.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ua.kishkastrybaie.repository.entity.Cart;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
+}
