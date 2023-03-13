6e8806d352986d5dfb50bb3b9679f74ca272b934
Andrey <pinchuk.andryusha310212@gmail.com>
2023-03-13 19:25:16 +0200

    feat: add ImageNameAlreadyExistsException

---
+++ src/main/java/ua/kishkastrybaie/image/ImageNameAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package ua.kishkastrybaie.image;
+
+public class ImageNameAlreadyExistsException extends RuntimeException {
+  public ImageNameAlreadyExistsException(String message) {
+        super(message);
+    }
+}
