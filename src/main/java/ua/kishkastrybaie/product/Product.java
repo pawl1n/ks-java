0637bd34480624a2e806af68fb10d7abb437688b
Andrey <pinchuk.andryusha310212@gmail.com>
2023-03-19 16:05:50 +0200

    feature: return response based on role

--- src/main/java/ua/kishkastrybaie/product/Product.java
+++ src/main/java/ua/kishkastrybaie/product/Product.java
@@ -1,16 +1,21 @@
 package ua.kishkastrybaie.product;
 
 import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
 import ua.kishkastrybaie.category.Category;
 import ua.kishkastrybaie.image.Image;
+import ua.kishkastrybaie.product.item.ProductItem;
 
 @Entity
 @Table(name = "product", schema = "main")
 @Getter
 @Setter
 public class Product {
+  @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+  private final Set<ProductItem> productItems = new HashSet<>();
   @Id
   @GeneratedValue(generator = "product_seq")
   @SequenceGenerator(
@@ -20,14 +25,10 @@ public class Product {
       allocationSize = 1)
   @Column(unique = true, nullable = false, updatable = false)
   private Long id;
-
   @Column(nullable = false)
   private String name;
-
   @Column private String description;
-
   @OneToOne(cascade = CascadeType.MERGE)
   private Image mainImage;
-
   @ManyToOne private Category category;
 }
